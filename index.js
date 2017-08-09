7f84bd9b73fa586c49d3c4347db1915235e8a34d
Josh Stillman <joshstillman@gmail.com>
2017-08-09 01:40:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  var nested = document.getElementById('nested')
+  return nested.querySelector('.target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+function increaseRankBy(n) {
+  var ranked = document.querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < ranked.length; i++){
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n
+  }
+  return
+}
